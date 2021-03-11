24a98f1d6db76a3fa82777aac71f1a3f098f856a
Curtis Scheffel <curtis.scheffel@galatea-associates.com>
2021-03-11 10:14:59 -0500

    Implemented caching to all three getHistoricalPriceMethods (with a known bug).  Changed API token to access more credits.

--- src/main/java/org/galatea/starter/service/IexService.java
+++ src/main/java/org/galatea/starter/service/IexService.java
@@ -1,7 +1,12 @@
 package org.galatea.starter.service;
 
+
 import java.time.LocalDate;
+import java.time.Year;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalUnit;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
@@ -16,6 +21,7 @@ import org.galatea.starter.domain.IexSymbol;
 import org.galatea.starter.domain.rpsy.IexHistoricalPriceEntityRspy;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 /**
  * A layer for transformation, aggregation, and business required when retrieving data from IEX.
@@ -106,19 +112,76 @@ public class IexService {
     log.info("Retrieving historical price data for Symbol: {}, range: {}", symbol, range);
 
     // Declare Variables
+    List<IexHistoricalPrice> historicalPrices;
+    List<IexHistoricalPriceEntity> historicalEntities;
+    LocalDate end = LocalDate.now();
+    LocalDate start;
 
-    // This was used to test the methods.  It works! :) -- I'll erase it when I finish.
-//    List<IexHistoricalPrice> prices = iexClientCloud.getHistoricalPrice(symbol, range);
-//    List<IexHistoricalPriceEntity> historicalEntities =
-//        IexHistoricalPriceEntity.createFromHistoricalPriceList(prices);
-//    historicalPriceEntityRspy.saveAll(historicalEntities);
-//
-//
-//    LocalDate end = LocalDate.of(2021,3,9);
-//    LocalDate start = LocalDate.of(2020,12,1);
-//    List<IexHistoricalPriceEntity> test = historicalPriceEntityRspy.findBySymbolAndDateBetween("FB",start, end);
-//    log.info(test.toString());
-    return iexClientCloud.getHistoricalPrice(symbol, range);
+    // Determine Range Boundaries
+    switch (range.toUpperCase()) {
+      case "MAX":
+        start = end.minusYears(15);
+        log.info(start.toString());
+        break;
+      case "5Y":
+        start = end.minusYears(5);
+        log.info(start.toString());
+        break;
+      case "2Y":
+        start = end.minusYears(2);
+        log.info(start.toString());
+        break;
+      case "1Y":
+        start = end.minusYears(1);
+        log.info(start.toString());
+        break;
+      case "YTD":
+        start = end.withDayOfYear(1);
+        log.info(start.toString());
+        break;
+      case "6M":
+        start = end.minusMonths(6);
+        log.info(start.toString());
+        break;
+      case "3M":
+        start = end.minusMonths(3);
+        log.info(start.toString());
+        break;
+      case "1M":
+        start = end.minusMonths(1);
+        log.info(start.toString());
+        break;
+      case "5D":
+        start = end.minusDays(5);
+        log.info(start.toString());
+        break;
+      default:
+        start = end;
+        log.info("Range entry does not match possible values.");
+    }
+
+    // Check database for symbols
+    /*
+      I need a more accurate way to test if the database has the symbols rather than isEmpty().
+      If I get a specific date and then try to run this method, if that date was within the last
+       30 days, it'll just return that.  How can I do this?
+     */
+    historicalEntities = historicalPriceEntityRspy.findBySymbolIgnoreCaseAndDateBetween(symbol, start, end);
+    if (historicalEntities.isEmpty()) {
+      log.info("Retrieving data from Iex Website and saving to database");
+      historicalPrices = iexClientCloud.getHistoricalPrice(symbol,range);
+
+      // Create list of entity objects
+      historicalEntities = IexHistoricalPriceEntity.createFromHistoricalPriceList(historicalPrices);
+
+      // Save entities in repo
+      historicalPriceEntityRspy.saveAll(historicalEntities);
+    } else {
+      log.info("Retrieving data from database");
+      historicalPrices = IexHistoricalPrice.createFromHistoricalEntityList(historicalEntities);
+    }
+
+    return historicalPrices;
   }
 
   /**
